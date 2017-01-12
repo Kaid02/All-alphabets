187ea8a170350855d3c9157d4924a381cba83a8b
Kaid02 <kabirS317@gmail.com>
2017-01-12 10:40:41 +0545

    Add files via upload

---
+++ alphabet.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+//#include<conio.h
+	int main()
+	{
+		int a,b;
+		a=65;
+		b=97;
+		while(a>=65&&a<=90)
+		{
+			printf("%c\t",a);
+		a++;
+	}
+	printf("\n");
+	while(b>=97&&b<=122)
+		{
+			printf("%c\t",b);
+		b++;
+	}
+	return 0;
+}
